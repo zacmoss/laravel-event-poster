d8f6dd56663cdc167364b3449bbab6edc8de61f3
potenza <zachary.moss@ymail.com>
2019-03-13 11:54:50 -0500

    Set up migrations

--- database/migrations/2019_03_13_164400_users.php
+++ database/migrations/2019_03_13_164400_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Users extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('cusers', function (Blueprint $table) {
-            $table->bigIncrements('id');
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id')->index();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('email');
         });
     }
 
@@ -31,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cusers');
+        Schema::dropIfExists('users');
     }
 }
--- resources/views/createEvent.blade.php
+++ resources/views/createEvent.blade.php
@@ -26,7 +26,27 @@
             <textarea type='text' name='description' placeholder="description"></textarea>
             <input type='submit' value="submit">
         </form>
+        <button onclick="notify()">Notify</button>
     </body>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            if (!Notification) {
+                alert('Desktop notifications not available in your browser. Try Chromium.'); 
+                return;
+            }
+
+            if (Notification.permission !== "granted") {
+                Notification.requestPermission();
+            }
+        });
+        let notify = () => {
+            let newNot = new Notification('A title', {
+                body: "Hey, this is a notification",
+            });
+        }
+
+    </script>
 
 
 </html>
